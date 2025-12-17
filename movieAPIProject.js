7c7b93f8acb8e82cd56a605d686acff022a7dcd2
Leonardo Gamboa <leogamboalg06@gmail.com>
2025-12-17 13:54:30 -0800

    Added features for adding and removing to favorites

--- movieAPIProject.js
+++ movieAPIProject.js
@@ -1,43 +1,87 @@
 const API_KEY = "f293dea9";
-console.log("API KEY:", API_KEY);
+let currentFavoriteMovies = 0;
+let movie;
+const favoriteGrid = document.getElementById("favoriteGrid");
+const cards = favoriteGrid.querySelectorAll(".gridCard");
 
-document.querySelector("form").addEventListener("submit", e => {
-    e.preventDefault();
-});
+document.querySelector("form").addEventListener("submit", getData);
 
 
-
-async function getData() {
+async function getData(e) {
+    e.preventDefault();  
     const movieName = document.getElementById("movieName").value;
 
-    const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${movieName}`;
+    const url = `https://www.omdbapi.com/?apikey=${API_KEY}&t=${movieName}`;
     try {
         const res = await fetch(url);
         const data = await res.json();
     // console.log(data);
 
     if (data.Response === "False") {
-      throw new Error(data.Error);
+        throw new Error(data.Error);
     }
 
-    // 3. Data existence check
-    if (!data.Search || data.Search.length === 0) {
-      throw new Error("No movies found");
-    }
+    // // 3. Data existence check
+    // if (!data.Search || data.Search.length === 0) {
+    //     throw new Error("No movies found");
+    // }
     
 
-    let firstMovie = document.getElementById("test");
-    let image = document.getElementById("testImg");
+    let firstMovie = document.getElementById("currentMovieName");
+    let image = document.getElementById("currentMovieImg");
     
-    console.log(data);
-    const movie = data.Search[0];
-    const name = movie.Title;
-    firstMovie.textContent = name;
-    image.src = movie.Poster;
+    console.log(data.Search);
+    movie = data;
+
+    if(movie.Poster === "N/A") {
+        image.src = "./images/noImageAvailable.png";
+    }   else{
+        image.src = movie.Poster;
+    }
+    image.onerror = () => {
+        image.src = "./images/noImageAvailable.png";
+        image.classList.add("placeholder");
+    };
+
+    image.style.display = "initial";
+    firstMovie.textContent = movie.Title;
     }
     catch (err) {
         console.error("Fetch failed:", err.message);
         alert("Movie not found!");
         return []; // prevents crashes
   }
+}
+
+function addToFavorites() {
+
+    if(currentFavoriteMovies===3) {
+    alert("Out of space!");
+    return;
+    }
+
+    const img = cards[currentFavoriteMovies].querySelector("img");  
+    if(movie.Poster === "N/A") {
+        img.src = "./images/noImageAvailable.png";
+    }   else{
+        img.src = movie.Poster;
+    }
+    img.onerror = () => {
+        img.src = "./images/noImageAvailable.png";
+        img.classList.add("placeholder");
+    };
+    img.style.display = "initial";
+    cards[currentFavoriteMovies++].querySelector(".favorite-title").textContent = movie.Title;
+}
+
+function removeFromFavorites(heart) {
+    const card = heart.closest(".gridCard");
+    const img = card.querySelector("img");
+    const title = card.querySelector(".favorite-title");
+
+    img.src = "";
+    img.style.display = "none";
+    title.textContent = "Movie Title";
+    img.classList.remove("placeholder");
+    currentFavoriteMovies = Math.max(0, currentFavoriteMovies - 1);
 }
