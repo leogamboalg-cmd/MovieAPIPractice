0501a48da8948b887033f2d0a26b5e2ce4633102
Leonardo Gamboa <leogamboalg06@gmail.com>
2025-12-16 19:23:48 -0800

    Update

--- movieAPIProject.js
+++ movieAPIProject.js
@@ -1,5 +1,21 @@
-// API KEY: http://www.omdbapi.com/?i=tt3896198&apikey=f293dea9
+let API_KEY = "f293dea9";
 
 document.querySelector("form").addEventListener("submit", e => {
     e.preventDefault();
 });
+
+const movieName = document.getElementById("movieName").value;
+
+const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${movieName}`;
+
+let firstMovie = document.getElementById("test");
+let image = document.getElementById("testImg");
+
+async function getData() {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    firstMovie.textContent = movieName;
+    const movie = data.Search[0];
+    image.src = movie.Poster;
+}
