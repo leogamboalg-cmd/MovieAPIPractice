8f828e01a347e59e2eab3c3f93d51cd66ad04fca
Leonardo Gamboa <leogamboalg06@gmail.com>
2025-12-16 23:53:18 -0800

    Adding error handling

--- movieAPIProject.js
+++ movieAPIProject.js
@@ -4,18 +4,39 @@ document.querySelector("form").addEventListener("submit", e => {
     e.preventDefault();
 });
 
-const movieName = document.getElementById("movieName").value;
 
-const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${movieName}`;
-
-let firstMovie = document.getElementById("test");
-let image = document.getElementById("testImg");
 
 async function getData() {
-    const res = await fetch(url);
-    const data = await res.json();
+    const movieName = document.getElementById("movieName").value;
+
+    const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${movieName}`;
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+    // console.log(data);
+
+    if (data.Response === "False") {
+      throw new Error(data.Error);
+    }
+
+    // 3. Data existence check
+    if (!data.Search || data.Search.length === 0) {
+      throw new Error("No movies found");
+    }
+    
+
+    let firstMovie = document.getElementById("test");
+    let image = document.getElementById("testImg");
+    
     console.log(data);
-    firstMovie.textContent = movieName;
     const movie = data.Search[0];
+    const name = movie.Title;
+    firstMovie.textContent = name;
     image.src = movie.Poster;
+    }
+    catch (err) {
+        console.error("Fetch failed:", err.message);
+        
+        return []; // prevents crashes
+  }
 }
